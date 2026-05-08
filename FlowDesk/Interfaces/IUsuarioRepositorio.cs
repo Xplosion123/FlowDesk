c85901787d9f20f6b1df72eb8355603e346cfe94
Henrique <henrimelo06@gmail.com>
2026-05-08 11:00:07 -0300

    feat: cria repositórios e interfaces

---
+++ FlowDesk/Interfaces/IUsuarioRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using FlowDesk.Models;
+
+namespace FlowDesk.Interfaces
+{
+    public interface IUsuarioRepositorio
+    {
+
+        // a interface não contem código apenas a promessa do que deve ser feito
+
+        LoginViewModel Validar(string Email, string Senha);
+
+        void CriarConta(LoginViewModel usuario);
+    }
+}
