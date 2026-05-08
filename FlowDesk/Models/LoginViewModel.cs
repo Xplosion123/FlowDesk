c5bc5936563a9367e69c56ab25fe9f96db99c41f
Henrique <henrimelo06@gmail.com>
2026-05-08 10:24:53 -0300

    feat: cria models

---
+++ FlowDesk/Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace FlowDesk.Models
+{
+    public class LoginViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Senha { get; set; } = string.Empty;
+
+        public string Nivel { get; set; } = string.Empty ;
+    }
+}
